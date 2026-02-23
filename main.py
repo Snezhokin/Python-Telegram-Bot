f9606eca1beec494c48448387d1584d3ffaa8f9d
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-23 22:35:54 +0300

    add: Main programm

--- main.py
+++ main.py
@@ -11,3 +11,13 @@ def bop (bot, update):
     url = get_url()
     chat_id = update.message.chat_id
     bot.send_photo(chat_id = chat_id , photo = url)
+
+def main():
+    updater = Updater('8503168222:AAHkWnmUUVWsxndQRbDKzrXMjJ6vaFHpsHA')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop', bop))
+    updater.start_polling()
+    updater.idle()
+
+if name == '__main__':
+    main()
