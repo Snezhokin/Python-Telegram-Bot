0c6ce3563fca27bbc29473682688c60a204ca0b1
Alexander Ledokhovich <alexander.ledokhich@mail.ru>
2026-02-23 22:09:08 +0300

    add: Function for accessing the api and getting the image url

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from telegram.ext import Updapter , CommandHandler
 import requests
 import re
 
+def get_url():
+    contens = requests.get('https://random.dog/woof.json').json()
+    url = contens['https://random.dog/']
+    return url
+
